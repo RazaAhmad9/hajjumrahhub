0d241677830db31b3b0f74d20c4551620f9d71e1
RazaAhmad9 <raza.ahmad.dev@gmail.com>
2025-03-16 22:06:59 +0500

    create umrah packages card

--- umrah-packages.php
+++ umrah-packages.php
@@ -1,4 +1,4 @@
-<!-- Container Start Here -->
+<!-- Banner Start Here -->
 <div
   class="up-banner up-<?php echo htmlspecialchars($page, ENT_QUOTES, 'UTF-8'); ?>"
   id="up-<?php echo htmlspecialchars($page, ENT_QUOTES, 'UTF-8'); ?>"
@@ -9,4 +9,69 @@
    
   </div>
 </div>
-<!-- Container Ends Here -->
+<!-- Banner Ends Here -->
+
+<!-- All Umrah Packages start Here -->
+<div id="up-all-umrah-packages" class="up-all-umrah-packages container">
+  <h2 class="up-h2-heading">All-inclusive Cheap Umrah Package</h2>
+  <p class="up-after-heading-text">03 Start Umrah Packages</p>
+  <div class="up-all-umrah-packages__wrapper">
+    <div class="up-all-umrah-packages__item">
+    <a href="#" class="up-umrah-package__link">
+        <img class="up-umrah-package__img" src="./images/3-Star-10-Nights-Economy-Umrah-Package.webp" alt="3 star 10 nights umrah package" width="283" height="235"/>
+      </a>
+      <h3 class="up-umrah-package__title"><a href="#">3 Star 10 Nights Umrah Package</a></h3>
+      <p class="up-umrah-package__tagline">Perform Umrah with Best Price</p>
+      <p class="up-umrah-package__price">£ 479</p>
+      <a href="#" class="up-umrah-package__btn">
+        <span class="up-svg-wrapper">
+          <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 92.2 122.88" style="enable-background:new 0 0 92.2 122.88" xml:space="preserve"><style type="text/css">.st0{fill-rule:evenodd;clip-rule:evenodd;}</style><g><polygon class="st0" points="92.2,60.97 0,122.88 0,0 92.2,60.97"/></g></svg>
+        </span>
+        <span> View Details</span>
+      </a>
+    </div>
+    <div class="up-all-umrah-packages__item">
+    <a href="#" class="up-umrah-package__link">
+        <img class="up-umrah-package__img" src="./images/3-Star-7-Nights-Umrah-Package.webp" alt="3 Star 7 Nights Umrah Package" width="283" height="235"/>
+      </a>
+      <h3 class="up-umrah-package__title"><a href="#">3 Star 7 Nights Umrah Package</a></h3>
+      <p class="up-umrah-package__tagline">Perform Umrah with Best Price</p>
+      <p class="up-umrah-package__price">£ 479</p>
+      <a href="#" class="up-umrah-package__btn">
+        <span class="up-svg-wrapper">
+          <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 92.2 122.88" style="enable-background:new 0 0 92.2 122.88" xml:space="preserve"><style type="text/css">.st0{fill-rule:evenodd;clip-rule:evenodd;}</style><g><polygon class="st0" points="92.2,60.97 0,122.88 0,0 92.2,60.97"/></g></svg>
+        </span>
+        <span> View Details</span>
+      </a>
+    </div>
+    <div class="up-all-umrah-packages__item">
+    <a href="#" class="up-umrah-package__link">
+        <img class="up-umrah-package__img" src="./images/3-Star-10-Nights-Economy-Umrah-Package.webp" alt="3 star 10 nights umrah package" width="283" height="235"/>
+      </a>
+      <h3 class="up-umrah-package__title"><a href="#">3 Star 10 Nights Umrah Package</a></h3>
+      <p class="up-umrah-package__tagline">Perform Umrah with Best Price</p>
+      <p class="up-umrah-package__price">£ 479</p>
+      <a href="#" class="up-umrah-package__btn">
+        <span class="up-svg-wrapper">
+          <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 92.2 122.88" style="enable-background:new 0 0 92.2 122.88" xml:space="preserve"><style type="text/css">.st0{fill-rule:evenodd;clip-rule:evenodd;}</style><g><polygon class="st0" points="92.2,60.97 0,122.88 0,0 92.2,60.97"/></g></svg>
+        </span>
+        <span> View Details</span>
+      </a>
+    </div>
+    <div class="up-all-umrah-packages__item">
+      <a href="#" class="up-umrah-package__link">
+        <img class="up-umrah-package__img" src="./images/3-Star-10-Nights-Economy-Umrah-Package.webp" alt="3 star 10 nights umrah package" width="283" height="235"/>
+      </a>
+      <h3 class="up-umrah-package__title"><a href="#">3 Star 10 Nights Umrah Package</a></h3>
+      <p class="up-umrah-package__tagline">Perform Umrah with Best Price</p>
+      <p class="up-umrah-package__price">£ 479</p>
+      <a href="#" class="up-umrah-package__btn">
+        <span class="up-svg-wrapper">
+          <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 92.2 122.88" style="enable-background:new 0 0 92.2 122.88" xml:space="preserve"><style type="text/css">.st0{fill-rule:evenodd;clip-rule:evenodd;}</style><g><polygon class="st0" points="92.2,60.97 0,122.88 0,0 92.2,60.97"/></g></svg>
+        </span>
+        <span> View Details</span>
+      </a>
+    </div>
+  </div>
+</div>
+<!-- All Umrah Packages Ends Here -->
